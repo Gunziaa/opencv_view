b6e473a469d65c4c54dbcba6e249b9c841c3f6f3
逆光行走的猫 <65211886+Gunziaa@users.noreply.github.com>
2023-06-08 00:15:39 +0800

    Initial commit

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-# 这是一个示例 Python 脚本。
+from module import matching, windowCapture
+from time import sleep
 
-# 按 Shift+F10 执行或将其替换为您的代码。
-# 按 双击 Shift 在所有地方搜索类、文件、工具窗口、操作和设置。
-
-
-def print_hi(name):
-    # 在下面的代码行中使用断点来调试脚本。
-    print(f'Hi, {name}')  # 按 Ctrl+F8 切换断点。
-
-
-# 按间距中的绿色按钮以运行脚本。
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    cap = windowCapture.WindowCapture('剑网3 - 绝代天骄 @ 电信八区(点卡区)')
+    matching = matching.Template(r'E:\Py\opencv_view\module\template.png')
+    shap = matching.w, matching.h
+    while True:
+        screenshot = cap.get_screenshot()
+        location = matching.matchingTemplate(screenshot)
+        matching.show_rect(screenshot, location, shap)
+        sleep(0.03)
 
-# 访问 https://www.jetbrains.com/help/pycharm/ 获取 PyCharm 帮助
+
